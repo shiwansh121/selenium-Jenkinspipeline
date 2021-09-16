08cd9169c462f17fda7b17c507add4201909d78b
shiwansh121 <88318244+shiwansh121@users.noreply.github.com>
2021-09-16 16:41:05 +0530

    Update Selenium.java

--- comtest/src/main/java/com/test/Selenium.java
+++ comtest/src/main/java/com/test/Selenium.java
@@ -2,7 +2,7 @@ package com.test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Seleniumtest {
+public class Selenium {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Desktop\\selenium\\chromedriver_win32\\chromedriver.exe"); // <-- Change this path
